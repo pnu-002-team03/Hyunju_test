8b8f075e55124c2bffbd26420c18eacc1d6eda07
user <user@IP-159>
2019-03-29 11:25:54 +0900

    First commit
    #TE3D-21

---
+++ src/InputOutputStream/OutputSTreamEX.java
@@ -0,0 +1,20 @@
+package InputOutputStream;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class OutputSTreamEX {
+    public static void main(String[] args) {
+	try {
+	    OutputStream output = new FileOutputStream("D:/Eclipse/Java/Output.txt");
+	    String str ="오늘 날씨는 아주 좋습니다.";
+	    byte[] by=str.getBytes();
+	    output.write(by);
+			
+	} catch (Exception e) {
+            e.getStackTrace();
+	
+		}
+	}
+  }
